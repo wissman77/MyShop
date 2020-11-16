448d09ad00acf74a975003f6444f75c5eb2769ce
wissman77 <43609409+wissman77@users.noreply.github.com>
2020-11-17 01:11:26 +0200

    Added Basket Tests

---
+++ MyShop.WebUI.Tests/Controllers/BasketControllerTest.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using MyShop.Core.ViewModels;
+using MyShop.Services;
+using MyShop.WebUI.Controllers;
+using System;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace MyShop.WebUI.Tests.Controllers
+{
+    [TestClass]
+    public class BasketControllerTest
+    {
+        [TestMethod]
+        public void CanAddBasketItem()
+        {
+            // setup
+            IRepository<Basket> baskets = new Mocks.MockContext<Basket>();
+            IRepository<Product> products = new Mocks.MockContext<Product>();
+
+            var httpContext = new Mocks.MockHttpContext();
+
+            IBasketService basketService = new BasketService(products, baskets);
+
+            var controller = new BasketController(basketService);
+            controller.ControllerContext = new System.Web.Mvc.ControllerContext(httpContext,
+                new System.Web.Routing.RouteData(), controller);
+
+            // Act
+            //basketService.AddToBasket(httpContext, "1");
+            controller.AddToBasket("1");
+
+            Basket basket = baskets.Collection().FirstOrDefault();
+
+            // Assert
+            Assert.IsNotNull(basket);
+            Assert.AreEqual(1, basket.BasketItems.Count);
+            Assert.AreEqual("1", basket.BasketItems.ToList().FirstOrDefault().ProductId);
+        }
+
+        [TestMethod]
+        public void CanGetSummaryViewModel()
+        {
+            // setup
+            IRepository<Basket> baskets = new Mocks.MockContext<Basket>();
+            IRepository<Product> products = new Mocks.MockContext<Product>();
+
+            products.Insert(new Product() { Id = "1", Price = 10.00m });
+            products.Insert(new Product() { Id = "2", Price = 5.00m });
+
+            Basket basket = new Basket();
+            basket.BasketItems.Add(new BasketItem() { ProductId = "1", Quantity = 2 });
+            basket.BasketItems.Add(new BasketItem() { ProductId = "2", Quantity = 1 });
+            baskets.Insert(basket);
+
+            IBasketService basketService = new BasketService(products, baskets);
+
+            var controller = new BasketController(basketService);
+            var httpContext = new Mocks.MockHttpContext();
+            httpContext.Request.Cookies.Add(new System.Web.HttpCookie("eCommerceBasket") { Value = basket.Id });
+
+            controller.ControllerContext = new System.Web.Mvc.ControllerContext(httpContext,
+                new System.Web.Routing.RouteData(), controller);
+
+            var result = controller.BasketSummary() as PartialViewResult;
+            var basketSummary = (BasketSummaryViewModel)result.ViewData.Model;
+
+            Assert.AreEqual(3, basketSummary.BasketCount);
+            Assert.AreEqual(25.00m, basketSummary.BasketTotal);
+        }
+    }
+}
--- MyShop.WebUI.Tests/Controllers/HomeControllerTest.cs
+++ MyShop.WebUI.Tests/Controllers/HomeControllerTest.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using MyShop.Core.ViewModels;
 using MyShop.WebUI;
 using MyShop.WebUI.Controllers;
 using System;
@@ -13,42 +16,19 @@ namespace MyShop.WebUI.Tests.Controllers
     public class HomeControllerTest
     {
         [TestMethod]
-        public void Index()
+        public void IndexPageDoesReturnProducts()
         {
-            // Arrange
-            //HomeController controller = new HomeController();
+            IRepository<Product> productContext = new Mocks.MockContext<Product>();
+            IRepository<ProductCategory> productCategoryContext = new Mocks.MockContext<ProductCategory>();
 
-            //// Act
-            //ViewResult result = controller.Index() as ViewResult;
+            productContext.Insert(new Product());
 
-            //// Assert
-            //Assert.IsNotNull(result);
-        }
+            HomeController controller = new HomeController(productContext, productCategoryContext);
 
-        [TestMethod]
-        public void About()
-        {
-            //// Arrange
-            //HomeController controller = new HomeController();
+            var result = controller.Index() as ViewResult;
+            var viewModel = (ProductListViewModel)result.ViewData.Model;
 
-            //// Act
-            //ViewResult result = controller.About() as ViewResult;
-
-            //// Assert
-            //Assert.AreEqual("Your application description page.", result.ViewBag.Message);
-        }
-
-        [TestMethod]
-        public void Contact()
-        {
-            //    // Arrange
-            //    HomeController controller = new HomeController();
-
-            //    // Act
-            //    ViewResult result = controller.Contact() as ViewResult;
-
-            //    // Assert
-            //    Assert.IsNotNull(result);
+            Assert.AreEqual(1, viewModel.Products.Count());
         }
     }
 }
